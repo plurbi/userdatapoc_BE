d14c36bda79db3f61ba8a73661d6e7600b527f0a
Patricio Luciano Urbieta <patricio.urbieta@gmail.com>
2023-08-03 16:22:27 -0300

    push

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  mongoURI: 'mongodb+srv://patricio:H7FyVAGCIAk4q50G@cluster0.lm55gkg.mongodb.net/',
-  secretKey: 'clave_secreta_para_jsonwebtoken'
+  mongoURI: 'mongodb+srv://patricio:H7FyVAGCIAk4q50G@cluster0.lm55gkg.mongodb.net/'
 };
 
--- src/controllers/purchaseController.js
+++ src/controllers/purchaseController.js
@@ -17,5 +17,51 @@ export const add = async (req, res) => {
 };
 
 export const get = async (req, res) => {
-  res.send([{}]);
+  try {
+    // Obtener todos los usuarios de la base de datos
+    const purchases = await PurchaseModel.find();
+    
+    console.log(purchases);
+
+
+    const grouped= transformData(purchases);
+
+    console.log(grouped);
+
+    // Enviar una respuesta al cliente
+    res.status(200).json(grouped);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
 }
+
+const transformData = (input) => {
+  const groupedData = {};
+
+  input.forEach((items, index) => {
+    console.log('items', items);
+    
+
+    const publicIp = items.publicIp;
+    const userToken = items.userToken;
+    const productToken = items.productToken;
+    if (!groupedData[publicIp]) {
+    
+      groupedData[publicIp] = {
+        key: String(index + 1),
+        PublicIp: publicIp,
+        children: [],
+      };
+    }
+
+    groupedData[publicIp].children.push({
+      key: String(index + 30),
+      UserToken: userToken,
+      ProductToken: productToken,
+      ProductName: "ProductName",
+    });
+  });
+
+  return Object.values(groupedData);
+};
